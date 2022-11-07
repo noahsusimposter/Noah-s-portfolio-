b2a2624f01266f6f4c0d24343f8ed2cb8be1c14a
noahsusimposter <2026shelton.noah@fcboe.org>
2022-11-07 14:41:08 -0500

    cacv2 update

--- c/calcV2.c
+++ c/calcV2.c
@@ -1,1 +1,21 @@
-fr
+#include <stdio.h>
+#include <stdlib.h>
+
+
+char operation;
+
+int number1 = 1;
+int number2 = 2;
+
+int main(void) {
+	printf("Operation (a, s, m, d): ");
+	scanf("%o\n", &operation);
+	printf("First Number: ");
+	scanf("%f\n", &number1);
+	printf("Second Number: ");
+	scanf("%s\n", &number2);
+	return 0;
+}
+
+int number1() {
+	printf(
---
+++ c/name.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+	char name[30];
+	printf("name: ");
+	scanf("%s", &name);
+	printf("Your name is %s\n", name);
+	return 0;
+}
