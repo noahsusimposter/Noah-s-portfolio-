685e0654bcd0761522c1699ca01d85f23e554947
noahsusimposter <110932966+noahsusimposter@users.noreply.github.com>
2022-11-07 14:51:20 -0500

    Update calcV2.c

--- c/calcV2.c
+++ c/calcV2.c
@@ -7,15 +7,40 @@ char operation;
 int number1 = 1;
 int number2 = 2;
 
-int main(void) {
+int main() {
 	printf("Operation (a, s, m, d): ");
 	scanf("%o\n", &operation);
-	printf("First Number: ");
-	scanf("%f\n", &number1);
-	printf("Second Number: ");
-	scanf("%s\n", &number2);
+	number1()
+	number2()
 	return 0;
 }
 
-int number1() {
-	printf(
+int number1(void) {
+	printf("First Number: ");
+	scanf("%f\n", &number1);
+}
+
+int number2(void) {
+	printf("Second Number: ");
+	scanf("%s\n", &number2);
+}
+	
+int exe(void) {
+	int final = 1;
+	if (operation = a); {
+	final = number1 + number2;
+	printf("Your awnser is: \n" final);
+	}
+	if (operation = s); {
+	final = number1 - number2;
+	printf("Your awnser is: \n" final);
+	}
+	if (operation = m); {
+	final = number1 * number2;
+	printf("Your awnser is: \n" final);
+	}
+	if (operation = d); {
+	final = number1 d number2;
+	printf("Your awnser is: \n" final);
+	}
+}
