20cd32d4af240de3bbe21481fc677f62464f1e74
noahsusimposter <110932966+noahsusimposter@users.noreply.github.com>
2022-11-07 15:42:38 -0500

    Update calcV2.c

--- c/calcV2.c
+++ c/calcV2.c
@@ -1,46 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
-char operation;
+char operation[1];
 
 int number1 = 1;
 int number2 = 2;
 
-int main() {
-	printf("Operation (a, s, m, d): ");
-	scanf("%o\n", &operation);
-	number1()
-	number2()
-	return 0;
-}
-
-int number1(void) {
-	printf("First Number: ");
-	scanf("%f\n", &number1);
-}
-
-int number2(void) {
+int num2(void) {
 	printf("Second Number: ");
-	scanf("%s\n", &number2);
+	scanf("%s", &number2);
+  printf("You chose %c\n", number2);
 }
-	
-int exe(void) {
+
+int num1(void) {
+	printf("First Number: ");
+	scanf("%f", &number1);
+  printf("You chose %c", number1);
+  num2();
+}
+
+
+
+
+int exe() {
 	int final = 1;
-	if (operation = a); {
+	if (strcmp(operation, "a") == 0) {
 	final = number1 + number2;
-	printf("Your awnser is: \n" final);
+	printf("Your awnser is: %a", final);
 	}
-	if (operation = s); {
+	if (strcmp(operation, "s") == 0) {
 	final = number1 - number2;
-	printf("Your awnser is: \n" final);
+	printf("Your awnser is: %a", final);
 	}
-	if (operation = m); {
+	if (strcmp(operation, "m") == 0) {
 	final = number1 * number2;
-	printf("Your awnser is: \n" final);
+	printf("Your awnser is: %a", final);
 	}
-	if (operation = d); {
-	final = number1 d number2;
-	printf("Your awnser is: \n" final);
+	if (strcmp(operation, "d") == 0) {
+	final = number1 / number2;
+	printf("Your awnser is: %a", final);
 	}
+}
+
+int main(void) {
+	printf("Operation (a, s, m, d): ");
+	scanf("%o", &operation);
+	num1();
 }
