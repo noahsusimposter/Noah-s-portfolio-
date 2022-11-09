54e2030d309fe90d786e53aeea2eace3230a588a
noahsusimposter <110932966+noahsusimposter@users.noreply.github.com>
2022-11-09 14:16:31 -0500

    Update calcV2.c

--- c/calcV2.c
+++ c/calcV2.c
@@ -9,45 +9,49 @@ char operation[1];
 int number1 = 1;
 int number2 = 2;
 
-int num2(void) {
-	printf("Second Number: ");
-	scanf("%s", &number2);
-  printf("You chose %c\n", number2);
-}
-
-int num1(void) {
-	printf("First Number: ");
-	scanf("%f", &number1);
-  printf("You chose %c", number1);
-  sleep(5);
-  num2();
-}
-
-
-
 
 int exe() {
 	int final = 1;
 	if (strcmp(operation, "a") == 0) {
 	final = number1 + number2;
-	printf("Your awnser is: %a", final);
+	printf("Your awnser is: %i\n", final);
 	}
 	if (strcmp(operation, "s") == 0) {
 	final = number1 - number2;
-	printf("Your awnser is: %a", final);
+	printf("Your awnser is: %i\n", final);
 	}
 	if (strcmp(operation, "m") == 0) {
 	final = number1 * number2;
-	printf("Your awnser is: %a", final);
+	printf("Your awnser is: %i\n", final);
 	}
 	if (strcmp(operation, "d") == 0) {
 	final = number1 / number2;
-	printf("Your awnser is: %a", final);
+	printf("Your awnser is: %i\n", final);
 	}
+  return 0;
 }
 
-int main(void) {
+
+int num2() {
+	printf("Second Number: ");
+	scanf("%i", &number2);
+  printf("You chose %i\n", number2);
+  exe();
+  return 0;
+}
+
+int num1() {
+	printf("First Number: ");
+	scanf("%i", &number1);
+  printf("You chose %i\n", number1);
+  sleep(5);
+  num2();
+  return 0;
+}
+
+int main() {
 	printf("Operation (a, s, m, d): ");
-	scanf("%o", &operation);
+	scanf("%c", &operation);
 	num1();
+  return 0;
 }
