8990889aef409859281f47e824f89d2e5c2bb1a1
Noah Shelton <2026shelton.noah@fcboe.org>
2022-12-12 14:38:57 -0500

    final stuff

--- final/invert.c
+++ final/invert.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 
 int main() {
-	
+	char str[20];	
+	printf("type a word: ");
+	scanf("%s", &str);
+  	for (int i = 0; str[i] != 0; i++) {
+		if (str[i] >= 'A' && str[i] <= 'Z') {
+			str[i] = str[i] + 32;
+		}
+		else if (str[i] >= 'a' && str[i] < 'z') {
+			str[i] = str[i] - 32;
+		}
+	}
+	printf("%s\n", str);
 
-
-return 0;
+	return 0;
 }
